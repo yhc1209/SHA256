c5f5e03b6ce9a3aa7b048bde7dc4ee2023dd661a
yhc1209 <82189968+yhc1209@users.noreply.github.com>
2021-06-07 00:01:56 +0800

    SHA256 testing 1

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace SHA256test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (SHA256 mySHA = SHA256.Create())
+            {
+                try
+                {
+                    byte[] hashValue = mySHA.ComputeHash(File.ReadAllBytes("C:\\Users\\abcd8\\Documents\\test2.txt"));
+                    
+                    foreach (byte b in hashValue)
+                        Console.Write($"{b:x}");
+                }
+                catch (IOException e) {
+                    Console.WriteLine($"I/O Exception: {e.Message}");
+                }
+                catch (UnauthorizedAccessException e) {
+                    Console.WriteLine($"Access Exception: {e.Message}");
+                }
+                Console.Write("\nPress any key to exit...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
